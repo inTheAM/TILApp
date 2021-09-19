215bada8646519cccb08e36f0d54380c13efcdb9
Ahmed Mgua <mguaahmed@gmail.com>
2021-09-19 18:58:43 +0300

    ADD:-	CreateUser migration

---
+++ Sources/App/Migrations/CreateUser.swift
@@ -0,0 +1,23 @@
+//
+//  File.swift
+//  
+//
+//  Created by Ahmed Mgua on 19/09/2021.
+//
+
+import Fluent
+
+struct CreateUser:	Migration	{
+	func prepare(on database: Database) -> EventLoopFuture<Void> {
+		database.schema("users")
+			.id()
+			.field("name", .string, .required)
+			.field("username", .string,	.required)
+			.create()
+	}
+	
+	func revert(on database: Database) -> EventLoopFuture<Void> {
+		database.schema("users")
+			.delete()
+	}
+}
