0e8e732ae092433f39b2dcac360ed8d26f15b071
Ahmed Mgua <mguaahmed@gmail.com>
2021-10-04 09:37:19 +0300

    ADD:- Extension on XCTApplicationTester adding login function and modifying test method to test auth-protected routes with loggedInRequest

--- Tests/AppTests/Extensions/Application+Testable.swift
+++ Tests/AppTests/Extensions/Application+Testable.swift
@@ -5,8 +5,8 @@
 //  Created by Ahmed Mgua on 21/09/2021.
 //
 
-import App
-import XCTVapor
+@testable import App
+@testable import XCTVapor
 
 extension Application {
 	static func testable() throws -> Application	{
@@ -18,4 +18,52 @@ extension Application {
 			.wait()
 		return app
 	}
+}
+
+extension XCTApplicationTester {
+    public func login(user: User) throws -> Token {
+        var request = XCTHTTPRequest(method: .POST, url: .init(path: "/api/users/login"), headers: [:], body: ByteBufferAllocator().buffer(capacity: 0))
+        request.headers.basicAuthorization = .init(username: user.username, password: "password")
+        let response = try performTest(request: request)
+        return try response.content.decode(Token.self)
+    }
+    
+    @discardableResult
+    public func test(_ method: HTTPMethod,
+                     _ path: String,
+                     headers: HTTPHeaders = [:],
+                     body: ByteBuffer? = nil,
+                     loggedInRequest: Bool = false,
+                     loggedInUser: User? = nil,
+                     file: StaticString = #file,
+                     line: UInt = #line,
+                     beforeRequest: (inout XCTHTTPRequest) throws -> () = {_ in },
+                     afterResponse: (XCTHTTPResponse) throws -> () = {_ in }
+    ) throws -> XCTApplicationTester {
+        var request = XCTHTTPRequest(method: method, url: .init(path: path), headers: headers, body: body ?? ByteBufferAllocator().buffer(capacity: 0))
+        
+        if (loggedInRequest || loggedInUser != nil) {
+            let userToLogin: User
+            if let user = loggedInUser {
+                userToLogin = user
+            } else {
+                userToLogin = User(name: "Admin", username: "admin", password: "password")
+            }
+            
+            let token = try login(user: userToLogin)
+            
+            request.headers.bearerAuthorization = .init(token: token.value)
+        }
+        
+        try beforeRequest(&request)
+        
+        do {
+            let response = try performTest(request: request)
+            try afterResponse(response)
+        } catch {
+            XCTFail("\(error)", file: (file), line: line)
+            throw error
+        }
+        return self
+    }
 }
